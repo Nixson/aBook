5e8d8fb03a51635ca475b51149e2a133fc3341cf
Плохотнюк Артем <aplokhotnyuk@diasoft.ru>
2019-02-11 17:40:10 +0300

    static + Rest

--- addressBook/src/main/java/ru/nixson/AddressBook.java
+++ addressBook/src/main/java/ru/nixson/AddressBook.java
@@ -9,31 +9,31 @@ import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public class AddressBook {
-    public void create(Book book){
+    public static void create(Book book){
         Session sess = SF.getInstance().openSession();
         sess.save(book);
         sess.close();
     }
 
-    public void update(Book book) {
+    public static void update(Book book) {
         Session sess = SF.getInstance().openSession();
         sess.update(book);
         sess.close();
     }
 
-    public void delete(int id){
+    public static void delete(int id){
         Session sess = SF.getInstance().openSession();
         Book book  = sess.byId(Book.class).load(id);
         sess.delete(book);
         sess.close();
     }
-    public Book byId(int id){
+    public static Book byId(int id){
         Session sess = SF.getInstance().openSession();
         Book book  = sess.byId(Book.class).load(id);
         sess.close();
         return book;
     }
-    public boolean auth(String login, String pass) throws NoSuchAlgorithmException {
+    public static Book auth(String login, String pass) throws NoSuchAlgorithmException {
         Session sess = SF.getInstance().openSession();
         Query q = sess.createQuery("FROM book where login = :login and password = :password");
         q.setParameter("login",login);
@@ -45,11 +45,11 @@ public class AddressBook {
         q.setParameter("password",myHash);
         int cnt = q.getFetchSize();
         if(cnt > 0){
-            return true;
+            return (Book)q.getSingleResult();
         }
-        return false;
+        return null;
     }
-    public List<Book> find(String attr){
+    public static List<Book> find(String attr){
         Session sess = SF.getInstance().openSession();
         Query query = sess.createQuery("from book where surname like '%:attr%' or" +
                 " firstname like '%:attr%' or " +
--- addressBook/src/main/java/ru/nixson/Front.java
+++ addressBook/src/main/java/ru/nixson/Front.java
@@ -1,4 +1,27 @@
 package ru.nixson;
 
-public class Front {
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.*;
+import java.security.NoSuchAlgorithmException;
+
+@ApplicationScoped
+@Path("/")
+public class Front implements FrontService{
+    @GET
+    @Path("auth")
+    @Override
+    public String auth (
+            @QueryParam("login") String login,
+            @QueryParam("password") String password
+    ) {
+        try{
+            Book ab = AddressBook.auth(login,password);
+            if(ab==null){
+                return "Ошибка авторизации";
+            }
+            return "";
+        } catch (NoSuchAlgorithmException err){
+            return "Ошибка";
+        }
+    }
 }
