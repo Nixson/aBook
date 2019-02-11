b83dacef07918e0f503f3e6054cc70e75cc07239
Плохотнюк Артем <aplokhotnyuk@diasoft.ru>
2019-02-11 14:39:41 +0300

    static

---
+++ webws/src/main/java/ru/nixson/Front.java
@@ -0,0 +1,13 @@
+package ru.nixson;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.List;
+
+@ManagedBean(name = "listbean")
+@RequestScoped
+public class Front {
+    public static void main(String[] args) {
+
+    }
+}
